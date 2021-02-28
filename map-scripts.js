041541c08e3012c53699fcd32613f1b7a16ca059
stevenschristopher <70532405+stevenschristopher@users.noreply.github.com>
2021-02-27 23:17:13 -0700

    added current location button, origin bttn notDone

--- map-scripts.js
+++ map-scripts.js
@@ -2,6 +2,8 @@ let map;
 var directionsService;
 var directionsRenderer;
 var geocoder;
+var currentPosLat;
+var currentPosLon;
 
 function initMap() {
     geocoder = new google.maps.Geocoder();
@@ -11,7 +13,44 @@ function initMap() {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
     });
+    
     directionsRenderer.setMap(map);
+}
+
+function getCurrentPosition() {
+/*****************************************************************************/
+    //code used for panning to the current location on the map
+    infoWindow = new google.maps.InfoWindow();
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+/*****************************************************************************/
+}
+
+function useCurPosAsOrigin() {
+    getCurrentPosition();
+    window.onload = function() {
+        document.getElementById("origin").innerHTML = "utah";
+    }
+    //alert("button clicked");
 }
 
 function calcRoute(event) {
@@ -39,6 +78,9 @@ function calcRoute(event) {
     });
 }
 
+
 window.onload = () => {
     document.getElementById('travel-form').addEventListener('submit', calcRoute);
+    document.getElementById('currentPositionBtn').addEventListener('click', getCurrentPosition);
+    document.getElementById('currentOriginBtn').addEventListener('click', useCurPosAsOrigin);
 }
